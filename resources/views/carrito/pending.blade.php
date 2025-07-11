c3526d3bb81018c51bca5210a3186be88482f70f
Alberto German Verissimo <germanverissimo5@hotmail.com>
2025-07-11 01:04:26 -0300

    Algunos estilo mas

--- resources/views/carrito/pending.blade.php
+++ resources/views/carrito/pending.blade.php
@@ -1,10 +1,20 @@
 <x-layout>
 
-         <x-slot:title>Mi Carrito-Pending</x-slot:title>
+    <x-slot:title>Mi Carrito - Pending</x-slot:title>
 
-    <div class="container mt-4">
-        <h1 class="mb-4 text-center">Pending</h1>
+    <div class="container mt-5">
+        <div class="text-center">
+            <h1 class="mb-4">Pago Pendiente</h1>
 
+            <div class="alert alert-warning mx-auto" style="max-width: 500px;">
+                Tu pago está pendiente de confirmación.
+                Por favor, espera mientras procesamos la información.
+            </div>
+
+            <a href="{{ route('producto.index') }}" class="btn btn-primary mt-3">
+                Volver a Productos
+            </a>
+        </div>
     </div>
 
 </x-layout>
--- resources/views/carrito/success.blade.php
+++ resources/views/carrito/success.blade.php
@@ -1,10 +1,20 @@
 <x-layout>
 
-         <x-slot:title>Mi Carrito-Success</x-slot:title>
+    <x-slot:title>Mi Carrito - Success</x-slot:title>
 
-    <div class="container mt-4">
-        <h1 class="mb-4 text-center">Success</h1>
+    <div class="container mt-5">
+        <div class="text-center">
+            <h1 class="mb-4">¡Gracias por tu compra!</h1>
 
+            <div class="alert alert-success mx-auto" style="max-width: 500px;">
+                Tu pedido ha sido procesado correctamente.
+                En breve recibirás un correo con los detalles de la transacción.
+            </div>
+
+            <a href="{{ route('producto.index') }}" class="btn btn-primary mt-3">
+                Seguir comprando
+            </a>
+        </div>
     </div>
 
 </x-layout>
